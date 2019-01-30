b0b20f8192785af5de2c6d05b311908e9ae98654
Jorge Usma <jusma@garantiascomunitarias.com>
2019-01-30 16:48:17 -0500

    pruebas

---
+++ application/view/test/editar.php
@@ -0,0 +1,6 @@
+<div class="container">
+    <h1>Test</h1>
+    <h2>You are in the View: application/view/editar/index.php (everything in this box comes from that file)</h2>
+    <!-- add song form -->
+    <!-- main content output -->
+</div>
