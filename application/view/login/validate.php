bdc5ed47da23fae24183c2655cde52ce59888cf8
Jorge Usma <jusma@garantiascomunitarias.com>
2019-01-30 17:13:11 -0500

    v4~

--- application/view/login/validate.php
+++ application/view/login/validate.php
@@ -3,4 +3,5 @@
     <h2>You are in the View: application/view/login/validate.php (everything in the box comes from this file)</h2>
     <p>In a real application this could be the homepage.</p>
     <p>otro cambioooo</p>
+    cambio nuevo...
 </div>
